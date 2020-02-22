fafe9dd7c9b8bb8562f9dfc0b786efed7b6a6946
Thales Martins <linkthales@hotmail.com>
2020-02-22 20:29:56 -0300

    Created Dashboard and Profile pages.

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 600px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      color: #fff;
+      background: rgba(0, 0, 0, 0.1);
+      height: 44px;
+      margin: 0 0 10px;
+      padding: 0 15px;
+      border: 0;
+      border-radius: 4px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    span {
+      align-self: flex-start;
+      color: #fb6f91;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+
+    button {
+      color: #fff;
+      background: #3b9eff;
+      height: 44px;
+      margin: 5px 0 0;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      font-weight: bold;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#3b9eff')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+
+  > button {
+    width: 100%;
+    color: #fff;
+    background: #f64c75;
+    height: 44px;
+    margin: 10px 0 0;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    font-weight: bold;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.08, '#f64c75')};
+    }
+  }
+`;
